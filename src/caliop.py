0b4ed08165ac53b265dd7cae79336f37f9937411
Vincent Meijer <vmeijer@mit.edu>
2023-05-25 11:11:00 -0400

    Add source code

---
+++ src/caliop.py
@@ -0,0 +1,267 @@
+import os, sys 
+import numpy as np
+import datetime as dt
+from typing import Type, List
+import matplotlib.pyplot as plt 
+
+import xarray as xr
+from pyhdf.SD import SD, SDC
+from pyhdf import HDF, VS
+
+import pandas as pd 
+from sklearn.neighbors import BallTree
+from skimage.measure import label, regionprops
+
+from cohesion.vertical_feature_mask import interpret_vfm
+from contrails.numerics.algorithms import *
+
+PROJECTION_PATH = '/net/d13/data/vmeijer/reprojections/'
+
+CALIOP_ALTITUDES = np.hstack((np.arange(40.0, 30.1, -0.300),
+                       np.arange(30.1, 20.1, -0.180),
+                       np.arange(20.1, 8.5, -0.060),
+                       np.arange(8.5, -0.5, -0.030)))
+
+
+class CollocationError(Exception):
+    """Exception raised for errors with collocation """
+    
+    pass
+
+
+class CALIOP:
+    """
+    Class to handle CALIOP file I/O
+    """
+    def __init__(self, path: str):
+        
+        self.path = path
+        self.file = SD(self.path, SDC.READ)
+        self.datasets = [ds for ds in self.file.datasets()]
+        self.time_bounds = self.get_time_bounds(UTC=True)
+        self.read_meta_data()
+        
+    def read_meta_data(self):
+
+        h4 =  HDF.HDF(self.path)
+        vs = h4.vstart()
+        vs_meta = vs.attach('metadata')
+        field_names = vs_meta.inquire()[2]
+
+        for i, name in enumerate(field_names):
+            setattr(self, name, vs_meta[0][i])
+        h4.close()
+
+    def get_time_bounds(self, UTC: bool=True) -> list:
+        """
+        Obtain initial and final time of CALIOP data
+        """
+        
+        if UTC:
+            time_key = "Profile_UTC_Time"
+        else:
+            time_key = "Profile_Time"
+        raw_time = self.file.select(time_key)[:]
+
+        time_bounds = [self._convert_time(t) for t in [raw_time[0,0], raw_time[-1, -1]]]
+        
+        return time_bounds      
+        
+    def _convert_time(self, t: int) -> dt.datetime:
+        """
+        Converts CALIPSO time format to python datetime object
+        Based on function found at: https://github.com/peterkuma/ccplot/blob/master/bin/ccplot
+        """
+            
+        d = int(t % 100)
+        m = int((t-d) % 10000)
+        y = int(t-m-d)
+
+        return dt.datetime(2000 + y//10000, m//100, d) + dt.timedelta(t % 1)
+    
+    def get_time(self, UTC: bool=True) -> np.array:
+        """
+        Get time dataset of .hdf file and convert to datetime
+        """ 
+
+        if UTC:
+            time_key = "Profile_UTC_Time"
+        else:
+            time_key = "Profile_Time"
+            
+        raw_time = self.file.select(time_key)[:]
+        result = [self._convert_time(t) for t in raw_time.flatten()]
+
+        return np.array(result).reshape(raw_time.shape)
+    
+    def available_dataset_names(self):
+        return self.datasets
+    
+    def get(self, dataset: str, with_units: bool=False) -> np.array:
+        """
+        Get dataset of .hdf file and fill values if applicable
+        """ 
+        
+        if dataset not in self.datasets:
+            raise KeyError(f"No dataset found corresponding to {dataset}")
+
+        if not with_units:
+            
+            # If there is a fillvalue, use this 
+            try:
+                fill_value = self.file.select(dataset).fillvalue
+                return np.ma.masked_equal(self.file.select(dataset)[:], fill_value)
+
+            except:
+                return self.file.select(dataset)[:]
+     
+        else:
+            fill_value = self.file.select(dataset).fillvalue
+            return np.ma.masked_equal(self.file.select(dataset)[:], fill_value), self.file.select(dataset)["units"]
+
+    def is_ascending(self):
+        lats = self.get("Latitude")
+        
+        if lats[1,0] - lats[0,0] > 0:
+            ascending = True
+        else:
+            ascending = False 
+
+        return ascending
+    
+    def filter_rows(self, lat_min, lat_max, cirrus=True):
+
+        lats = self.get("Latitude")
+        fcfs = self.get("Feature_Classification_Flags")
+        extinction_qcs = self.get("ExtinctionQC_532")
+        top_alts = self.get("Layer_Top_Altitude")
+
+        # Find coordinates of endpoints of (sub-)trajectory
+        # Column 0 contains locations corresponding to start of a 5km segment
+        # Column 2 contains locations corresponding to end of a 5km segment
+        if self.is_ascending():
+            rows = np.where((lats[:,0] >= lat_min)*(lats[:,2] <= lat_max))[0]
+        else:
+            rows = np.where((lats[:,2] >= lat_min)*(lats[:,0] <= lat_max))[0]
+
+        # Filter rows based on feature classification flags and extinction quality control
+        rows_to_keep = []
+        for r in rows[~top_alts.mask[rows, 0]]:
+            fcf = fcfs[r, 0]
+            extinctionqc = extinction_qcs[r,0]
+
+            if not filter_feature(fcf) or extinctionqc >= 16:
+                continue 
+
+            rows_to_keep.append(r)
+
+        if not cirrus:
+            rows_to_keep = rows[~np.isin(rows, rows_to_keep)]
+
+        return rows_to_keep
+
+
+def filter_feature(fcf: int) -> bool:
+    """
+    Given a feature classification flag from the CALIOP L2 layer product, determines whether the feature is cirrus cloud
+    """
+    
+    interpreted = interpret_vfm(fcf)
+
+    conditions = [interpreted[0] == "cloud", interpreted[1] == "high",
+                  interpreted[2] in ["randomly oriented ice", "horizontally oriented ice"],
+                  interpreted[3] == "high"]
+
+    return all(conditions)
+
+
+def collocate(lon_caliop: np.array, lat_caliop: np.array, lons_goes: np.array, lats_goes: np.array) -> tuple:
+    R = 6378e3
+    distance = (R*np.cos(np.radians(lat_caliop))*(np.radians(lon_caliop)-np.radians(lons_goes)))**2 + \
+                     (R*(np.radians(lat_caliop) - np.radians(lats_goes)))**2
+    idx = np.argmin(distance) 
+    
+    if distance[idx] < 2000:
+        
+        return lons_goes[idx], lats_goes[idx], idx
+    else:
+        return np.nan, np.nan, 0    
+
+    
+def interpolate_caliop_profile(data, lidar_alts=CALIOP_ALTITUDES, ve2=40.0e3, ve1=0.0, vres=30.0):
+    """
+    Interpolates a CALIOP L1 profile to a regular grid.
+
+    Parameters
+    ----------
+    data: np.array
+        Data to interpolate
+    lidar_alts: np.array (optional)
+        The altitude (MSL) corresponding to the rows of 'data', in km
+    ve2: float (optional)
+        Vertical coordinate of top of interpolated grid in meters
+    ve1: float (optional)
+        Vertical coordinate of bottom of interpolated grid in meters
+    vres: float (optional)
+        The vertical resolution to interpolate to
+
+    Returns
+    -------
+    interpolated: np.array
+        Interpolated data
+    """
+
+    e1 = 0
+    e2 = data.shape[0]
+    
+    X = np.arange(e1, e2, dtype=np.float32)
+    Y = np.meshgrid(1000*lidar_alts, X)[0].astype(np.float32)
+    data = np.array(np.ma.masked_equal(data,  -9999).astype(np.float32))
+    interpolated = interp2d_12(data.astype(np.float32), X, Y, float(e1), float(e2), int(e2-e1), ve2, ve1,
+                       int((ve2-ve1)/vres))
+    
+    return interpolated
+
+
+def subset_caliop_profile(caliop, dataset_name, extent,
+                             return_coords=False):
+    """
+    Subset a particular CALIOP profile to a geodetic extent.
+
+    Parameters
+    ----------
+    caliop: CALIOP
+        CALIOP object containing data to subset
+    dataset_name: str
+        The name of the variable to subset
+    extent: list
+        List of minimum longitude, maximum longitude, minimum latitude,
+        maximum latitude in degrees
+    return_coords: bool (optional)
+        Option to return longitudes, latitudes, times of subsetted profile
+    
+    Returns
+    -------
+    subset_data: np.array
+        Subsetted data
+    """
+    
+    # Extract longitudes, latitudes and UTC times
+    lons = caliop.get("Longitude")
+    lats = caliop.get("Latitude")
+    times = caliop.get_time()
+    
+    # Extract data
+    data = caliop.get(dataset_name)
+    
+    # Now apply subsetting
+    subset_mask = (lons >= extent[0])*(lons <= extent[1])\
+                    *(lats >= extent[2])*(lats <= extent[3])
+    subset_data = data[subset_mask[:,0], :]
+    
+    if return_coords:
+        return subset_data, lons[subset_mask], lats[subset_mask], times[subset_mask]
+        
+    else:
+        return subset_data
+    
---
+++ src/geometry.py
@@ -0,0 +1,565 @@
+import numpy as np
+from scipy.optimize import bisect, newton
+import os
+
+from contrails.satellites.goes.abi import *
+
+GRS80_PARAMS = {"b": 6356752.314140347, "a": 6378137.}
+GOES16_PARAMS= {"h": 35786.0234375*1000., "lon_0": -75.2}
+RADIUS_EARTH = 6378. # km
+
+
+class GroundTrack:
+
+    def __init__(self, lons, lats, n_segments=5):
+        self.lons = lons
+        self.lats = lats
+        if lats[0] > lats[-1]:
+            self.ascending = False
+        else:
+            self.ascending = True
+        self.n_segments = n_segments
+        self.setup_segments()
+    
+    def setup_segments(self):
+
+        lat_per_segment = (self.lats[-1]-self.lats[0])/self.n_segments
+
+        # First segment
+        approx_lat = lat_per_segment + self.lats[0]
+        # Find index
+        idx = np.argmin(np.abs(self.lats-approx_lat))
+        self.segments = [GreatCirclePath(self.lons[0], self.lats[0], self.lons[idx],
+                            self.lats[idx])]
+
+        for n in range(1, self.n_segments-1):
+            lon0 = self.lons[idx]
+            lat0 = self.lats[idx]
+
+            # Find new idx
+            approx_lat = lat0 + lat_per_segment
+            # Find index
+            idx = np.argmin(np.abs(self.lats-approx_lat))
+            self.segments.append(GreatCirclePath(lon0, lat0, self.lons[idx], self.lats[idx]))
+        
+        # Final segment
+        self.segments.append(GreatCirclePath(self.lons[idx], self.lats[idx], self.lons[-1], self.lats[-1]))
+
+    
+    def get_coordinates(self, n_points=100):
+
+        # Divide n_points over segments
+        points_per_segment = int(n_points/self.n_segments)
+   
+        points = []
+
+        for i in range(self.n_segments):
+            points.append(self.segments[i].get_coordinates(n_points=points_per_segment))
+
+        stacked = np.hstack(points)
+        return stacked[0,:], stacked[1,:]
+
+    def get_crosstrack_distance(self, lon, lat):
+
+        if isinstance(lon, float):
+
+            # Find relevant segment
+            for seg in self.segments:
+                
+                if (seg.lat0 <= lat)*(seg.lat1 >= lat) and self.ascending:
+                    break
+                if (seg.lat0 >= lat)*(seg.lat1 <= lat) and not self.ascending:
+                    break
+
+            return seg.get_crosstrack_distance(lon, lat)
+        
+        else:
+            # Find relevant segment
+            dists = np.nan * np.zeros_like(lat)
+            for seg in self.segments:
+                if self.ascending:
+                    idx = (seg.lat0 <= lat)*(seg.lat1 >= lat)
+                else:
+                    idx = (seg.lat0 >= lat)*(seg.lat1 <= lat)
+                    
+                dists[idx] = seg.get_crosstrack_distance(lon[idx], lat[idx])
+            
+            return dists
+
+class GreatCirclePath:
+    """
+    Represents a great circle path between two points. 
+
+    Parameters
+    ----------
+    lon0: float
+        Longitude of first point, degrees
+    lat0: float
+        Latitude of first point, degrees
+    lon1: float
+        Longitude of second point, degrees
+    lat1: float
+        Latitude of second point, degrees
+
+    """
+    
+    def __init__(self, lon0, lat0, lon1, lat1):
+        
+        self.lon0 = lon0
+        self.lat0 = lat0
+        self.lon1 = lon1
+        self.lat1 = lat1
+    
+    def get_crosstrack_distance(self, lon, lat):
+        """
+        Compute distance from points specified by (lon, lat) to great circle.
+        Inputs in degrees. 
+        """ 
+        
+        bearing_start_end = np.radians(get_bearing(self.lon0, self.lat0, self.lon1, self.lat1))
+        bearing_start_third = np.radians(get_bearing(self.lon0, self.lat0, lon, lat))
+        
+        angular_distance = get_angular_distance(self.lon0, self.lat0, lon, lat)
+        
+        dist = np.arcsin(np.sin(angular_distance)*np.sin(bearing_start_third-bearing_start_end))*RADIUS_EARTH
+        return dist
+    
+    def get_coordinates(self, n_points=100):
+        """
+        Sample 'n_points' along great circle path.
+        """ 
+        t = np.linspace(0, 1, n_points)
+        return great_circle_intermediate_point(self.lon0, self.lat0, self.lon1, self.lat1, t)
+        
+
+def ECEF2geodetic(x):
+    """
+    Converts a vector in the Earth Centered Earth Fixed (ECEF) coordinate frame
+    to spherical coordinates
+
+    Parameters
+    ----------
+    x: np.array
+        ECEF coordinate vector
+    
+    Returns
+    -------
+    lon: float
+        Longitude in degrees
+    lat: float
+        Latitude in degrees
+    """
+    lat = np.degrees(np.arcsin(x[2,:]))
+    lon = np.degrees(np.arctan2(x[1,:], x[0,:]))
+    return lon, lat
+
+
+def get_bearing(lon0, lat0, lon1, lat1): 
+    """
+    Computes bearing of line extending from (lon0, lat0) to (lon1, lat1).
+    
+    Parameters
+    ----------
+    lon0: float
+        Longitude of first point, degrees
+    lat0: float
+        Latitude of first point, degrees
+    lon1: float
+        Longitude of second point, degrees
+    lat1: float
+        Latitude of second point, degrees
+    
+    Returns
+    -------
+    bearing: float
+        Bearing in degrees
+    """ 
+    a1 = np.sin(np.radians(lon1-lon0))*np.cos(np.radians(lat1))
+    a2 = np.cos(np.radians(lat0))*np.sin(np.radians(lat1)) - np.sin(np.radians(lat0))\
+        *np.cos(np.radians(lat1))*np.cos(np.radians(lon1-lon0))
+    
+    return np.degrees(np.arctan2(a1, a2))
+
+
+def get_haversine(lon0, lat0, lon1, lat1) -> np.array:
+    """
+    Computes length of greatcircle path extending from (lon0, lat0) to (lon1, lat1).
+    
+    Parameters
+    ----------
+    lon0: float
+        Longitude of first point, degrees
+    lat0: float
+        Latitude of first point, degrees
+    lon1: float
+        Longitude of second point, degrees
+    lat1: float
+        Latitude of second point, degrees
+    
+    Returns
+    -------
+    haversine: float
+        Haversine distance in km
+    """ 
+    return RADIUS_EARTH*get_angular_distance(lon0, lat0, lon1, lat1)
+   
+        
+def get_angular_distance(lon0, lat0, lon1, lat1):
+    """
+    Computes central angle between great circle path extending from (lon0, lat0)
+    to (lon1, lat1).
+    
+    Parameters
+    ----------
+    lon0: float
+        Longitude of first point, degrees
+    lat0: float
+        Latitude of first point, degrees
+    lon1: float
+        Longitude of second point, degrees
+    lat1: float
+        Latitude of second point, degrees
+    
+    Returns
+    -------
+    angular distance: float
+        Central angle in radians 
+    """ 
+    a = np.sin(0.5*np.radians(lat1-lat0))**2 \
+        + np.cos(np.radians(lat0))*np.cos(np.radians(lat1)) \
+            * np.sin(0.5*np.radians(lon1-lon0))**2
+
+    angular_distance = 2 * np.arctan2(np.sqrt(a), np.sqrt(1-a))
+    return angular_distance
+
+
+def parallax_correction_vicente_forward(lon, lat, h,
+                            globe_params=GRS80_PARAMS, sat_params=GOES16_PARAMS):
+    """
+    Implements the parallax correction method by Vicente et al. (2002) as
+    described in Bielinski (2020). 
+
+    Parameters
+    ----------
+    lon: float
+        Longitude, degrees
+    lat: float
+        Latitude, degrees
+    h: float
+        Cloud height, meters
+    globe_params: dict (optional)
+        The ellipsoid used for geodetic coordinates
+    sat_params: dict (optional)
+        The satellite used for the correction.
+
+    Returns
+    -------
+    lon_correct: float
+        Corrected longitude, degrees
+    lat_correct: float
+        Corrected latitude, degrees
+    """
+    # Unpack parameters
+    R_eq = globe_params["a"]
+    R_pol = globe_params["b"]
+    
+    e = np.sqrt(R_eq**2-R_pol**2)/R_eq
+    
+    h_s = sat_params["h"]
+    sat_lon = sat_params["lon_0"]
+    
+    
+    R_equivalent = R_eq/np.sqrt(np.cos(np.radians(lat))**2 \
+                                + (R_eq**2/R_pol**2)*np.sin(np.radians(lat))**2)
+    
+    # geocentric latitude
+    glat = np.degrees(np.arctan((1-e**2)*np.tan(np.radians(lat))))
+    
+    # Apparent cloud position
+    Xc = R_equivalent*np.cos(np.radians(glat))*np.cos(np.radians(lon-sat_lon))
+    Yc = R_equivalent*np.cos(np.radians(glat))*np.sin(np.radians(lon-sat_lon))
+    Zc = R_equivalent*np.sin(np.radians(glat))
+
+    # Satellite position
+    Xs = R_eq + h_s
+    Ys = 0
+    Zs = 0
+
+    # Difference vector
+    Xcs = Xs - Xc
+    Ycs = Ys - Yc
+    Zcs = Zs - Zc
+
+   
+    # Solve equation
+    if isinstance(lat, float):
+        # Determine equation to be solved
+        f = lambda c: (((Xc+c*Xcs)**2 + (Yc+c*Ycs)**2)/(R_eq + h)**2) + ((Zc + c*Zcs)**2/(R_pol +h)**2) - 1
+        c = bisect(f, 0, 1)
+    else:
+        c = np.zeros_like(lat)
+        for i in range(c.size):
+            # Determine equation to be solved
+            f = lambda c: (((Xc[i]+c*Xcs[i])**2 + (Yc[i]+c*Ycs[i])**2)/(R_eq + h[i])**2) + ((Zc[i] + c*Zcs[i])**2/(R_pol +h[i])**2) - 1
+            try:
+                c[i] = bisect(f, 0, 1)
+            except ValueError:
+                print(f(0), f(1))
+                raise ValueError
+            
+
+    # Cloud top coordinates
+    Xt = Xc + c*Xcs
+    Yt = Yc + c*Ycs
+    Zt = Zc + c*Zcs
+
+    # Convert to geodetic
+    lat_correct = np.degrees(np.arctan(((R_eq)**2/(R_pol)**2) * Zt/(np.sqrt(Xt**2 + Yt**2)) ) )
+    lon_correct = np.degrees(np.arctan(Yt/Xt)) + sat_lon
+    
+    return lon_correct, lat_correct
+
+
+def parallax_correction_vicente_backward(lon, lat, h, globe_params=GRS80_PARAMS,
+                                                     sat_params=GOES16_PARAMS):
+    
+    # Unpack parameters
+    R_eq = globe_params["a"]
+    R_pol = globe_params["b"]
+    
+    h_s = sat_params["h"]
+    sat_lon = sat_params["lon_0"]
+    
+    e = np.sqrt(R_eq**2-R_pol**2)/R_eq
+    
+    R_equivalent = (R_eq+h)/np.sqrt(np.cos(np.radians(lat))**2 \
+                                + ((R_eq+h)**2/(R_pol+h)**2)*np.sin(np.radians(lat))**2)
+    
+    glat = np.degrees(np.arctan((1-e**2)*np.tan(np.radians(lat))))
+    # Cloud position
+    Xt = R_equivalent*np.cos(np.radians(glat))*np.cos(np.radians(lon-sat_lon))
+    Yt = R_equivalent*np.cos(np.radians(glat))*np.sin(np.radians(lon-sat_lon))
+    Zt = R_equivalent*np.sin(np.radians(glat))
+
+    # Satellite position
+    Xs = R_eq + h_s
+    Ys = 0
+    Zs = 0
+
+    # Difference vector
+    Xts = Xt - Xs
+    Yts = Yt - Ys
+    Zts = Zt - Zs
+
+    # Solve equation
+    if isinstance(lat, float) or isinstance(lat,int):
+        # Determine equation to be solved
+        f = lambda c: ((Xt+c*Xts)**2 + (Yt+c*Yts)**2)/(R_eq**2) + ((Zt + c*Zts)**2/(R_pol**2)) - 1
+        f_prime = lambda c: 2*(Xts*(Xt+c*Xts)+Yts*(Yt+c*Yts))/R_eq**2 + 2*Zts*(Zt+c*Zts)/R_pol**2 
+        
+        c = newton(f, -1, fprime=f_prime)
+    else:
+        c = np.zeros_like(lat)
+        for i in range(c.size):
+
+            # Determine equation to be solved                                    
+            f = lambda c: ( ((Xt[i]+c*Xts[i])**2 + (Yt[i]+c*Yts[i])**2)/(R_eq**2)) + ((Zt[i] + c*Zts[i])**2/(R_pol**2)) - 1
+            f_prime = lambda c: 2*(Xts[i]*(Xt[i]+c*Xts[i])+Yts[i]*(Yt[i]+c*Yts[i]))/R_eq**2 + 2*Zts[i]*(Zt[i]+c*Zts[i])/R_pol**2 
+            try:
+                c[i] = newton(f, -1, fprime=f_prime)
+            except (ValueError, RuntimeError):
+                print("Backward parallax correction failed")
+                print(f"longitude: {lon[i]}, latitude: {lat[i]}, height: {h[i]}")
+                print(f"F(-1): {f(-1)}, F(0): {f(0)}")
+                raise ValueError
+                
+
+    # Apparent cloud top coordinates
+    Xc = Xt + c*Xts
+    Yc = Yt + c*Yts
+    Zc = Zt + c*Zts
+
+    # Convert to geodetic
+    lat_correct = np.degrees(np.arctan( (R_eq**2/(R_pol**2) * Zt/(np.sqrt(Xc**2 + Yc**2)) ) ))
+    lon_correct = np.degrees(np.arctan(Yc/Xc)) + sat_lon
+    
+    return lon_correct, lat_correct
+
+
+def geodesic_distance(lon1, lat1, lon2, lat2, globe_params=GRS80_PARAMS,
+                         max_iter=100, tol=1e-6):
+    """
+    Uses Vicenty's formula to compute the geodesic distance between two
+    geodetic points. This is more accurate than the Haversine distance, which
+    assumes a spherical earth.
+
+    Parameters
+    ----------
+    lon1: float
+        Longitude of first point, degrees
+    lat1: float
+        Latitude of first point, degrees
+    lon2: float
+        Longitude of second point, degrees
+    lat2: float
+        Latitude of second point, degrees
+    globe_params: dict (optional)
+        The ellipsoid used for geodetic coordinates
+    max_iter: int (optional)
+        The maximum number of iterations to perform
+    tol: float (optional)
+        The convergence criterion
+
+    Returns
+    -------
+    s: float
+        Distance in meters
+    """
+
+    a = globe_params["a"]
+    b = globe_params["b"]
+    f = (a-b)/a
+    
+    U1 = np.arctan((1-f)*np.tan(np.radians(lat1)))
+    U2 = np.arctan((1-f)*np.tan(np.radians(lat2)))
+    
+    lamda = np.radians(lon2-lon1)
+    converged = False
+    it = 0
+    
+    while not converged and it < max_iter:
+
+        cos_sigma = np.sin(U1)*np.sin(U2) + np.cos(U1) * np.cos(U2) * np.cos(lamda)
+        sigma = np.arccos(cos_sigma)
+        
+        sin_alpha = np.cos(U1)*np.cos(U2)*np.sin(lamda)/np.sin(sigma)
+        
+        cos_2sigma_m = np.cos(sigma)-2*np.sin(U1)*np.sin(U2)/(1-sin_alpha**2)
+        C = (f/16)*(1-sin_alpha**2) * (4+f*(4-3*(1-sin_alpha**2)))
+        lamda_old = lamda
+        lamda = np.radians(lon2-lon1) + (1-C)*f*sin_alpha*(sigma \
+               + C*np.sin(sigma) * (cos_2sigma_m + C*np.cos(sigma)*(-1 + 2 * cos_2sigma_m**2)))
+        
+        if np.all(np.abs(lamda_old - lamda)/lamda_old < tol):
+            converged = True
+        it += 1
+        
+    u_sq =  (1-sin_alpha**2) * (a**2-b**2)/b**2
+    A = 1 + u_sq/(16384) * ( 4096 + u_sq * (-768 + u_sq * (320 - 175*u_sq)))
+    B = u_sq/1024 * (256 + u_sq *(-128 + u_sq * (74 - 47*u_sq)))
+    dsigma = B*np.sin(sigma)*(cos_2sigma_m + (B/4)*(np.cos(sigma)*(-1 + 2*cos_2sigma_m**2))
+                             - (B/6)*cos_2sigma_m *(-3 + 4 * np.sin(sigma)**2)*(-3 + 4*cos_2sigma_m**2))
+    s = b*A*(sigma-dsigma)
+    
+    return s 
+                   
+def get_ABI_grid_locations(x, y, dx=5.5998564e-05, dy=5.5998564e-05):
+    cols = np.floor(x/dx).astype(np.int64) + 2712
+    rows = -np.floor(y/dy).astype(np.int64) + 2711
+    return rows, cols
+
+
+def great_circle_intermediate_point(lon1, lat1, lon2, lat2, fraction):
+    """
+    Compute an intermediate point along the great circle path between
+    (lon1, lat1) and (lon2, lat2), along 'fraction' of the distance.
+    
+    Parameters
+    ----------
+    lon1: float
+        Longitude of first point, degrees
+    lat1: float
+        Latitude of first point, degrees
+    lon2: float
+        Longitude of second point, degrees
+    lat2: float
+        Latitude of second point, degrees    
+    
+    Returns
+    -------
+    lon_i: float
+        Longitude of point, degrees
+    lat_i: float
+        Latitude of point, degrees
+    """
+    
+    d = get_angular_distance(lon1, lat1, lon2, lat2)
+    a = np.sin((1-fraction)*d)/np.sin(d)
+    b = np.sin(fraction*d)/np.sin(d)
+    x = a * np.cos(np.radians(lat1))*np.cos(np.radians(lon1)) \
+            + b * np.cos(np.radians(lat2))*np.cos(np.radians(lon2))
+    y = a * np.cos(np.radians(lat1))*np.sin(np.radians(lon1)) \
+            + b * np.cos(np.radians(lat2))*np.sin(np.radians(lon2))
+    z = a * np.sin(np.radians(lat1)) + b * np.sin(np.radians(lat2))
+    
+    lat_i = np.degrees(np.arctan2(z, np.sqrt(x**2 + y**2)))
+    lon_i = np.degrees(np.arctan2(y, x))
+    
+    return lon_i, lat_i
+
+
+# def get_look_angles(lon: float, lat: float, alt: float,
+#             GOES16_h: float=35786.0234375, GOES16_lon0: float=-75.2) -> tuple:
+#     """
+#     Computes the look angles for a geostationary satellite.
+#     Reference: https://geodesy.noaa.gov/CORS/Articles/SolerEisemannJSE.pdf
+
+#     Parameters
+#     ----------
+#     lon: float
+#         Longitude in degrees
+#     lat: float
+#         Latitude in degrees
+#     alt: float
+#         Altitude of object/cloud in km
+#     nc: xr.Dataset
+#         GOES ABI dataset
+
+#     Returns
+#     -------
+#     viewing_angle: float
+#         The viewing angle (complement of the zenith angle) in degrees
+#     azimuth_angle: float
+#         The azimuth angle (defined w.r.t. North) in degrees
+#     """
+#     assert np.all(lat) > 0
+
+#     # Earth radius in km
+#     R_e = 6371.
+#     lon_difference = abs(lon - GOES16_lon0)
+#     lat_difference = abs(lat)
+
+
+#     # Eq. 1 in reference
+#     cos_gamma = np.cos(np.radians(lon_difference)) * np.cos(np.radians(lat_difference))
+#     gamma = np.arccos(cos_gamma)
+
+#     # Eq. 2 in reference
+#     r = R_e + GOES16_h
+#     R = R_e + alt
+
+#     s = r*np.sqrt(1 + (R/r)**2 -2*(R/r)*np.cos(gamma))
+
+#     # Eq. 3 & 4 in reference
+#     sinndr = (R / s) * np.sin(gamma)
+#     sinzen = ((R + GOES16_h) / R) * sinndr
+
+#     viewing_angle = np.degrees(np.arcsin(sinzen))
+
+#     # Eq. 6 in reference
+#     beta = np.arccos(np.tan(np.radians(lat_difference))/np.tan(gamma))
+
+#     # See Fig. 2 in reference
+#     # (we assume all points are north of sub-sat point, see assert)
+
+#     # Point east of satellite subpoint
+#     idx = lon > GOES16_lon0
+#     azimuth_angle =  180 - np.degrees(beta)
+#     azimuth_angle[idx] = 180 + np.degrees(beta[idx])
+
+#     return viewing_angle, azimuth_angle
+
+
+def get_scan_angle(zenith_angle,  GOES16_h: float=35786.0234375, R_e=6371.):
+    
+    return np.degrees(np.arcsin( (R_e/(R_e+GOES16_h))*np.sin(np.radians(zenith_angle))))
