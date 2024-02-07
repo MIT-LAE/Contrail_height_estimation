b81f70d248349002a71f7a2cc62b24559275f287
Vincent Meijer <vmeijer@mit.edu>
2024-02-07 16:50:03 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
-from setuptools import setup
+from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy
 
-setup(ext_modules=cythonize("CAP/interpolation.pyx"),
-        include_dirs=[numpy.get_include()])
+extension = Extension("CAP.interpolation", 
+                ["CAP/interpolation.pyx"],
+                libraries=[],
+                include_dirs=[numpy.get_include()])
+
+setup(ext_modules=cythonize(extension))
