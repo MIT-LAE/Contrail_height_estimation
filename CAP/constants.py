4a070630a57ba49e60ed4ff3770300d9155f434b
Vincent Meijer <vmeijer@mit.edu>
2024-02-07 14:37:47 -0500

    Update constants

--- CAP/constants.py
+++ CAP/constants.py
@@ -3,4 +3,4 @@
 GRS80_PARAMS = {"b": 6356752.314140347, "a": 6378137.} # Both in meters
 
 # Mean radius of the Earth based on GRS80 system
-RADIUS_EARTH = 0.5 * (GRS80_PARAMS["a"] + GRS80_PARAMS["b"])# meters
+RADIUS_EARTH = 0.5 * (GRS80_PARAMS["a"] + GRS80_PARAMS["b"]) # meters
