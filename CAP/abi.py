a18456593b1087bede45d5aae8b39c4628811aab
Vincent Meijer <vmeijer@mit.edu>
2024-02-07 14:18:10 -0500

    Update documentation

--- CAP/abi.py
+++ CAP/abi.py
@@ -2,23 +2,39 @@ import numpy as np, datetime as dt
 import xarray as xr
 import cartopy.crs as ccrs
 
+from .constants import GRS80_PARAMS
 from .utils import *
 
 
 
-GOES16_PARAMS= {"h": 35786.0234375*1000., "lon_0": -75.2}
+GOES16_PARAMS = {"h": 35786.0234375*1000., "lon_0": -75.2}
 
-# location of the CONUS upper left corner in the Full disk ABI grid
+# Location of the CONUS product upper left corner in the Full Disk ABI grid
+# Obtained from page 26 in 
+# https://www.goes-r.gov/products/docs/PUG-L2+-vol3.pdf
+# PUG Volume 3: Level 1b Products
+# DCN 7035538, Revision H.1
 CONUS_FIRST_COL = 902
 CONUS_FIRST_ROW = 422
 
-# Extent and projection for the Orthographic projection centered at CONUS 
+# Extent and projection for the orthographic projection centered at CONUS
+# introduced in Kulik (2019): https://dspace.mit.edu/handle/1721.1/124179
 ORTHO_EXTENT = [-2373970.2054802035,3638399.906890716, -2388159.0372323524,
-                 1620933.528091551]  
+                 1620933.528091551]
 ORTHO_PROJ = ccrs.Orthographic(central_latitude=39.8283,
                                  central_longitude=-98.5795)
 
+# Amount of rows and columns in the orthographic projection centered at CONUS
+# introduced in Kulik (2019): https://dspace.mit.edu/handle/1721.1/124179
+# see page 18
+N_ORTHO_ROWS = 2000
+N_ORTHO_COLS = 3000
 
+# Table A1. from Carr et al. (2020)
+# https://www.mdpi.com/2072-4292/12/22/3779
+# contains the time offsets of the ABI bands in second
+# i.e. [TIME_OFFSET_CHANNEL_1, TIME_OFFSET_CHANNEL_2, ...,
+# TIME_OFFSET_CHANNEL_16]
 GOES16_BAND_OFFSETS = [0.179, -0.055, 0.402, 0.642, -0.359, -0.642,	0.535,
             0.267, 0.000, -0.267, -0.535, -0.542, 0.551, 0.319, -0.256, 0.579]
                 
@@ -50,25 +66,29 @@ def ABI2geodetic(x, y, nc=None):
         proj_info = nc['goes_imager_projection']
         
         lon_origin = proj_info.longitude_of_projection_origin
-        H = proj_info.perspective_point_height+proj_info.semi_major_axis
+        H = proj_info.perspective_point_height + proj_info.semi_major_axis
         r_eq = proj_info.semi_major_axis
         r_pol = proj_info.semi_minor_axis
     else:
+        # Values for `lon_origin` and `H` are from the reference mentioned
+        # in the function docstring
         lon_origin = -75.0 # degrees
-        r_eq = 6378137.0 # meters
-        r_pol = 6356752.31414 # meters
+        r_eq = GRS80_PARAMS["a"] # meters
+        r_pol = GRS80_PARAMS["b"] # meters
         H  = 42164160.0 # meters
     
-    a = np.sin(x)**2 + np.cos(x)**2 * (np.cos(y)**2 + (r_eq**2/r_pol**2)*np.sin(y)**2)
-    b = -2*H*np.cos(x)*np.cos(y)
+    a = np.sin(x)**2 + np.cos(x)**2 * (np.cos(y)**2 \
+            + (r_eq**2 / r_pol**2) * np.sin(y)**2)
+    b = -2 * H * np.cos(x) * np.cos(y)
     c = H**2 - r_eq**2
-    r_s = (-b-np.sqrt(b**2 - 4*a*c))/(2*a)
-    s_x = r_s*np.cos(x)*np.cos(y)
-    s_y = -r_s*np.sin(x)
-    s_z = r_s*np.cos(x)*np.sin(y)
+    r_s = (-b - np.sqrt(b**2 - 4 * a * c)) / (2 * a)
+    s_x = r_s * np.cos(x) * np.cos(y)
+    s_y = -r_s * np.sin(x)
+    s_z = r_s * np.cos(x) * np.sin(y)
 
-    lat = np.degrees(np.arctan((r_eq**2/r_pol**2) * s_z/(np.sqrt((H-s_x)**2 + s_y**2))))
-    lon = lon_origin - np.degrees(np.arctan(s_y/(H-s_x)))
+    lat = np.degrees(np.arctan((r_eq**2 / r_pol**2) \
+                                    * s_z / (np.sqrt((H - s_x)**2 + s_y**2))))
+    lon = lon_origin - np.degrees(np.arctan(s_y / (H - s_x)))
     
     return lon, lat
 
@@ -94,6 +114,8 @@ def geodetic2ABI(lon, lat, nc=None):
     y: float
         ABI fixed grid y-coordinate, radians
     """
+    # Value for eccentricity `e` is from the reference mentioned in the
+    # docstring
     if nc is not None:
         proj_info = nc['goes_imager_projection']
         
@@ -103,9 +125,11 @@ def geodetic2ABI(lon, lat, nc=None):
         r_pol = proj_info.semi_minor_axis
         e = 0.0818191910435
     else:
+        # Values for `lon_origin` and `H` are from the reference mentioned
+        # in the function docstring
         lon_origin = -75.0 # degrees
-        r_eq = 6378137.0 # meters
-        r_pol = 6356752.31414 # meters
+        r_eq = GRS80_PARAMS["a"] # meters
+        r_pol = GRS80_PARAMS["b"] # meters
         H  = 42164160.0 # meters
         e = 0.0818191910435
     
@@ -125,6 +149,22 @@ def geodetic2ABI(lon, lat, nc=None):
     return x, y
 
 def map_geodetic_extent_to_ABI(extent, conus=False):
+    """
+    Utility function to map a geodetic extent to ABI product rows and columns
+
+    Parameters
+    ----------
+    extent: list
+        Geodetic extent [lon_min, lon_max, lat_min, lat_max]
+    conus: bool
+        Whether the extent should be mapped to CONUS product or Full-disk
+        rows and columns
+
+    Returns
+    -------
+    ABI_extent: list
+        ABI extent [col_min, col_max, row_min, row_max]
+    """
     x_ul, y_ul = geodetic2ABI(extent[0], extent[3])
     x_ll, y_ll = geodetic2ABI(extent[0], extent[2])
 
@@ -146,26 +186,37 @@ def map_geodetic_extent_to_ABI(extent, conus=False):
     return [max(c_min,0), max(c_max,0), max(r_min,0), max(r_max,0)]
 
 
-def get_ABI_grid_locations(x, y, dx=5.5998564e-05, dy=5.5998564e-05):
+def get_ABI_grid_locations(x, y, dx=56e-6, dy=56e-6):
     """
-    Finds row and column of given ABI fixed-grid coordinates
-
+    Finds rows and columns in ABI full disk 2 km grid
+    given `x` and `y` scan angles.
+    The values `dx` and `dy` are obtained from table 5.1.2.4 in the GOES-16
+    Product Definition and User's Guide, Version 3: Level 1b product.
+    # DCN 7035538, Revision H.1
     Parameters
     ----------
-    x : Union[float, np.array]
-        ABI fixed-grid x coordinate, in radians
-    y : Union[float, np.array]
-        ABI fixed-grid y coordinate, in radians
-    
+    x: float
+        ABI fixed grid x-coordinate, radians
+    y: float
+        ABI fixed grid y-coordinate, radians
+    dx: float, optional
+        ABI fixed grid x-coordinate resolution, radians
+    dy: float, optional
+        ABI fixed grid y-coordinate resolution, radians
     Returns
     -------
-    rows : Union[int, np.array]
-        ABI fixed-grid rows
-    cols : Union[int, np.arary]
-        ABI fixed-grid columns
+    rows: np.array
+        Rows in ABI grid corresponding to `x` and `y`
+    cols: np.array
+        Columns in ABI grid corresponding to `x` and `y`
     """
-    cols = np.floor(x/dx).astype(np.int64) + 2712
-    rows = -np.floor(y/dy).astype(np.int64) + 2711
+    cols = np.floor(x/dx).astype(np.int64) + N_ABI_FD_COLS // 2
+    
+    # To understand the - 1, refer to figure 5.1.2.6-2 
+    # in the GOES-16
+    # Product Definition and User's Guide, Version 3: Level 1b product.
+    # DCN 7035538, Revision H.1
+    rows = -np.floor(y/dy).astype(np.int64) + (N_ABI_FD_ROWS // 2) - 1
     return rows, cols
 
 
@@ -194,7 +245,10 @@ def get_pixel_times(scan_mode, band, region="FD"):
     if scan_mode not in list(range(1,7)):
         raise ValueError("Scan mode should be between 1 and 6")
     
-
+    # Load NetCDF file containing pixel scan times
+    # these files were found in the Supplementary Materials of 
+    # Carr et al. (2020):
+    # https://www.mdpi.com/2072-4292/12/22/3779#app1-remotesensing-12-03779
     ds = get_netcdf_asset(f"mode{scan_mode}")
 
     times = ds[region +"_pixel_times"].values
@@ -217,24 +271,76 @@ def get_pixel_times(scan_mode, band, region="FD"):
 
 
 def get_scan_start_time(goes_time, scan_mode, product):
+    """
+    For a particular scan mode and product, returns the time at which the
+    scan started.
+
+    Parameters
+    ----------
+    goes_time: datetime
+        The GOES-16 product time. For full-disk, this is a round time of 10
+        or 15 minutes (e.g. 10:00, 10:15, 10:30, 10:45). For CONUS,
+        this is a round time of 5 minutes (e.g. 10:00, 10:05, 10:10, 10:15)
+    scan_mode: int
+        The scan mode
+    product: str
+        The ABI product, either "FD" or "CONUS{1,2,3}" with the latter number
+        indicating which of the three CONUS products is being used. CONUS1
+        is the first CONUS product obtained during a particular FD scan, etc.
     
+    Returns
+    -------
+    start_time: datetime
+        The time at which the scan started
+    """
     
+    # Scan mode 3 corresponds to 1 Full disk product every 15 minutes
     if scan_mode == 3:
-        product_time = floor_time(goes_time, minute_res=15)
+        product_time = floor_time(g oes_time, minute_res=15)
         if "CONUS" in product:
+            
             conus_n = int(product[-1])
-            return product_time + dt.timedelta(minutes=(2+(conus_n-1)*5),seconds=21, milliseconds=700)
+            
+            # 21 second delta is based on looking at the observation
+            # start times for GOES-16 ABI-L2-MCMIPC products when scan mode 3
+            # was active. These start times are found in the filenames. 
+            # They tend to fluctuate by a few seconds with a 21 second mean.
+            # We add 5 minutes for every CONUS product after the first
+            offset = dt.timedelta(minutes=(2 + (conus_n-1)*5),
+                        seconds=21)
+            return product_time + offset
             
         else:
-            return product_time + dt.timedelta(seconds=40, milliseconds=700)
+            # 40 second delta is based on looking at the observation
+            # start times for GOES-16 ABI-L2-MCMIPF products when scan mode 3
+            # was still active. These start times are found in the filenames. 
+            # They tend to fluctuate by < 5 seconds with a 40 second mean
+            return product_time + dt.timedelta(seconds=40)
      
     # Scan mode 6
     else:
         product_time = floor_time(goes_time, minute_res=10)
         if "CONUS" in product:
             conus_n = int(product[-1])
-            return product_time + dt.timedelta(minutes=(1+(conus_n-1)*5),seconds=18, milliseconds=300)
+
+            # 17 second delta is based on looking at the observation
+            # start times for GOES-16 ABI-L2-MCMIPC products when scan mode 6
+            # is active. These start times are found in the filenames. 
+            # They tend to fluctuate by a few seconds with a 17 second mean
+            # See https://www.ospo.noaa.gov/Operations/GOES/east/g16meso.html
+            # for the up-to-date scheduling, and an explanation for these
+            # fluctuations
+            # We add 5 minutes for every CONUS product after the first
+            return product_time + dt.timedelta(minutes=(1+(conus_n-1)*5),
+                        seconds=17)
             
         else:
-            return product_time + dt.timedelta(seconds=21, milliseconds=600)
+            # 20 second delta is based on looking at the observation
+            # start times for GOES-16 ABI-L2-MCMIPF products when scan mode 6
+            # is active. These start times are found in the filenames. 
+            # They tend to fluctuate by a few seconds with a 20 second mean
+            # See https://www.ospo.noaa.gov/Operations/GOES/east/g16meso.html
+            # for the up-to-date scheduling, and an explanation for these
+            # fluctuations
+            return product_time + dt.timedelta(seconds=20)
         
