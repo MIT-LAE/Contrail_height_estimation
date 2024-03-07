e9d0732e75493dbf0dc6519741fe2e17de7d2016
Vincent Meijer <vmeijer@mit.edu>
2024-03-07 04:46:05 -0500

    Switch to click for CLI

--- scripts/coarse_L1_collocation.py
+++ scripts/coarse_L1_collocation.py
@@ -10,12 +10,15 @@ Use this script to do a coarse collocation between CALIOP L1 products
 and contrail detections.
 """
 
-import os, sys, glob
-import numpy as np
+import os
+import sys
+import glob
 import multiprocessing 
 
-
-sys.path.append("/home/vmeijer/contrail-height-estimation/")
+import click
+import numpy as np
+import pandas as pd
+import datetime as dt
 
 from CAP.caliop import *
 from CAP.geometry import * 
@@ -34,7 +37,7 @@ def get_mask(time, conus=False):
         if conus:
             suffix = "C"
         path = "/net/d13/data/vmeijer/data/orthographic_detections_goes16/" \
-                + "ABI-L2-MCMIP" + suffix + time.strftime("/%Y/%j/%H/%Y%m%d_%H_%M.csv")
+                    + "ABI-L2-MCMIP" + suffix + time.strftime("/%Y/%j/%H/%Y%m%d_%H_%M.csv")
         try:
             df = pd.read_csv(path) 
         except FileNotFoundError as e:
@@ -44,15 +47,15 @@ def get_mask(time, conus=False):
         mask[df.row.values, df.col.values] = 1
         return mask
     else:
-        df = pd.read_csv("/home/vmeijer/covid19/data/predictions_wo_sf/" + time.strftime('%Y%m%d.csv'))
+        df = pd.read_csv("/home/vmeijer/covid19/data/predictions_wo_sf/"\
+                            + time.strftime('%Y%m%d.csv'))
         df.datetime = pd.to_datetime(df.datetime)
         df = df[df.datetime == time]
         mask = np.zeros((2000, 3000))
         mask[df.x.values, df.y.values] = 1
         return mask
 
-
-def main(input_path, save_path):
+def process_file(input_path, save_path):
 
     if os.path.exists(save_path):
         print(f"Already done for {input_path}, result at {save_path}")
@@ -65,17 +68,24 @@ def main(input_path, save_path):
         print(f"Failed for {input_path} with {str(e)}")
         return
 
-if __name__ == "__main__":
-    paths = ["/home/vmeijer/contrail-height-estimation/data/CAL_LID_L1-Standard-V4-10.2018-08-08T18-58-40ZD_Subset.hdf"]
-    
+@click.command()
+@click.argument("input_path", type=click.Path(exists=True))
+@click.argument("save_dir", type=click.Path())
+@click.option("--debug", is_flag=True, default=False)
+def main(input_path, save_dir, debug):
+    paths = pd.read_csv(input_path, header=None)[0].values
+
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
     save_paths = []
     for p in paths:
-        save_paths.append(os.path.join(SAVE_DIR,
+        save_paths.append(os.path.join(save_dir,
                     os.path.basename(p).replace(".hdf", SUFFIX)))
-
-    if sys.argv[-1] == "DEBUG":
+        
+    if debug:
         for p, s in zip(paths, save_paths):
-            main(p, s)
+            process_file(p, s)
     else:
         n_cpus = int(os.environ.get("SLURM_CPUS_PER_TASK", 1))
 
@@ -83,5 +93,9 @@ if __name__ == "__main__":
 
         print(f"Running {__file__} in parallel using {n_cpus} CPUs")
 
-        pool.starmap(main, zip(paths, save_paths))
+        pool.starmap(process_file, zip(paths, save_paths))
         pool.close()
+    
+
+if __name__ == "__main__":
+    main()
