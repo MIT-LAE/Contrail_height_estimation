b1b7ee00b58617e4f02ea403251441b7d6974853
Vincent Meijer <vmeijer@mit.edu>
2024-03-18 13:20:18 -0400

    WIP on flight altitude distributions

--- scripts/create_flight_altitude_distributions.py
+++ scripts/create_flight_altitude_distributions.py
@@ -24,7 +24,8 @@ from shapely.ops import unary_union
 
 from contrails.meteorology.era5 import get_ERA5_data
 from contrails.flights.utils import (DomainBoundary, get_flight_data,
-                                    subset_flight_data)
+                                    subset_flight_data, get_haversine)
+
 from CAP.afca import (prepare_AFCA_input_flights,
                       prepare_AFCA_backward_analysis, SECONDS_IN_HOUR,
                       OODSENTINEL)
@@ -42,8 +43,6 @@ RADIUS_EARTH = 6371e3 # m
 # Altitude limits for flight data
 MIN_ALTITUDE = 8000 # m
 MAX_ALTITUDE = 15e3 # m
-
-
 
 def get_uncertainty_ellipse(longitude, latitude, time_advected,
                             dV=MAX_WIND_ERROR):  
@@ -106,10 +105,6 @@ def get_flight_candidates(longitude, latitude, time,
     era5 = get_ERA5_data(time)
     pressure_levels = era5.isobaricInhPa.values
 
-    polygons = []
-    sub_polygons = []
-    afca_outs = []
-
     n_press = len(pressure_levels)
     result = pd.DataFrame({"start_longitude" : [longitude] * n_press,
                         "start_latitude" : [latitude] * n_press,
@@ -126,7 +121,8 @@ def get_flight_candidates(longitude, latitude, time,
                                         pressure_levels, n_hours=hour, step=1)
             
             afca_inp.to_csv(AFCA_input_path)
-            # TODO: Remove ERA5 file directory and get this from function instead
+            # TODO: Remove ERA5 file directory and get this from
+            # function instead
             cmd = (f"{AFCA_EXECUTABLE} {AFCA_input_path} "
                 f"/net/d15/data/vmeijer/ERA5/{time.strftime('%Y/%Y_%m_%d.nc')}"
                 f" {AFCA_output_path} --ood-sentinel")
@@ -166,17 +162,24 @@ def get_flight_candidates(longitude, latitude, time,
     polygon_union = unary_union(list(result.polygon_1.values) \
                                 + list(result.polygon_2.values))
 
+    # Create DomainBoundary object from the union
     db_flights = DomainBoundary(*np.column_stack(
                 polygon_union.convex_hull.exterior.xy).T, great_circle=True)
-
+    
+    # Load OpenSky flight data
     flights = get_flight_data(time)
+
+    # Rename columns
     flights = flights.rename(columns={"lat" : "latitude",
                                       "lon" : "longitude",
                                       "alt" : "altitude",
                                       "velocity" : "speed"})
 
+    # Drop NaN values in specified columns
     flights = flights.dropna(subset=["longitude", "latitude", "time",
                                         "heading", "speed", "callsign"])
+    
+    # Perform subsetting to obtain flight candidates
     flights = subset_flight_data(pd.DataFrame(flights),
                             boundary=db_flights,
                             min_altitude=MIN_ALTITUDE,
@@ -217,143 +220,50 @@ def forward_advect_flights(flights, time):
     return flights
 
 
+def get_flight_altitude_distribution(flights):
+    """
+    For the flight data within `flights`, finds the distance flown in km
+    on every 10th flight level (FL).
+
+    Parameters
+    ----------
+    flights : pd.DataFrame
+        Flight data
     
-def process_FL_dist(path):
+    Returns
+    -------
+    distance_distribution : pd.DataFrame
+        Distance flown on every 10th flight level (FL), in km
+    """
+
+    def get_segment_length(x):
+        
+        # Obtain distances between waypoints in `x` in km
+        dists = get_haversine(x["longitude"].values.astype(np.float64)[1:],
+                        x["latitude"].values.astype(np.float64)[1:],
+                        x["longitude"].shift(1).values.astype(np.float64)[1:],
+                        x["latitude"].shift(1).values.astype(np.float64)[1:])
     
-    res = pd.read_pickle('test_set_glad-resonance.pkl')
-    
-    conus = False
-    if "MCMIPC" in path:
-        conus = True
-
-    time = dt.datetime.strptime("_".join(os.path.basename(path).split("_")[1:4]), "%Y%m%d_%H_%M")
-    min_col, max_col, min_row, max_row = [int(s) for s in os.path.basename(path).split(".")[0].split("_")[-4:]]
-    label = np.load(path.replace("images", "labels"))
-
-    rows, cols = np.where(label > 0)
-    rows += min_row
-    cols += min_col
-
-
-    suffix = "C" if conus else "F"
-    nc = xr.open_dataset(get_nc_path(time, product="ABI-L2-MCMIP" + suffix))
-
-    x = nc.x.values[cols]
-    y = nc.y.values[rows]
-
-    lons, lats = ABI2geodetic(x, y)
-    
-
-    from collections import defaultdict
-
-    itp_coords = {"longitude" : xr.DataArray(lons, dims="s"),
-                  "latitude" : xr.DataArray(lats, dims="s"),
-                  "time" : xr.DataArray(np.array([time] * len(lons)), dims="s")}
-
-    era5 = get_ERA5_data(time)
-    
-    gh_vals = era5.z.interp(**itp_coords, method="linear").values
-
-    pressure_df = defaultdict(list)
-
-    keys = list(res.columns)
-    for rm in ["error", "pe", "ape", "filename"]:
-        keys.remove(rm)
-
-    counter = 0
-    for _, row in res[res.filename == path].iterrows():
-
-        pressures = get_contrail_pressures(row[keys] * 1000, gh_vals[counter,:] / 9.80665, era5.isobaricInhPa.values)
-        counter += 1
-        for k, v in zip(keys, pressures):
-
-            pressure_df[k].append(v)
-
-    pressure_df = pd.DataFrame(pressure_df)
-    FL_df = pres2alt(pressure_df * 100) / 0.3048 / 100
-    FL_df.to_parquet("/home/vmeijer/contrail-height-estimation/data/FL_distributions/" \
-                    + os.path.basename(path).replace(".png", ".parquet"),
-                    )
-    
-    
-    
-def process_dist_distribution(path):
-    
-    
-    save_path = "/home/vmeijer/contrail-height-estimation/data/flight_distributions/" \
-        + os.path.basename(path).replace(".npy",".parquet")
-    
-    def dist_func(x):
-    
-        dist = np.hstack((0, get_haversine(x["longitude"].values.astype(np.float64)[1:],
-                                           x["latitude"].values.astype(np.float64)[1:],
-                                           x["longitude"].shift(1).values.astype(np.float64)[1:],
-                                           x["latitude"].shift(1).values.astype(np.float64)[1:])))
-
-        x["dist_km"] = dist
+        # Add this information is a new column
+        # Prepend a 0 to account for the first waypoint
+        x["dist_km"] = np.hstack((0, dists))
         return x
-    
-    filtered_flights = pd.read_parquet(save_path)
-                     
-    filtered_flights = filtered_flights.groupby("icao24", group_keys=False).apply(dist_func)
 
-    filtered_flights["FL"] = filtered_flights["altitude"] / 0.3048 / 100
-    filtered_flights["FL_bin"] = 10 * np.round(filtered_flights["FL"].values.astype(np.float32)/10)
+    flights = flights.groupby("icao24",
+                        group_keys=False).apply(get_segment_length)
 
-    hist_dist = filtered_flights.groupby("FL_bin")[["dist_km"]].sum()
-    hist_dist.to_parquet(save_path.replace("flight", "distance"))
+    # / 0.3048 for conversion meters to feet
+    # / 100 for conversion feet to flight level (FL)
+    flights["FL"] = flights["altitude"] / 0.3048 / 100
     
-    
-def process_weather_data(path):
-    
-    conus = False
-    if "MCMIPC" in path:
-        conus = True
+    # Round flight levels to the nearest 10th
+    flights["FL_bin"] = 10 * np.round(flights["FL"].values.astype(np.float32)\
+                                      /10)
 
-    time = dt.datetime.strptime("_".join(os.path.basename(path).split("_")[1:4]), "%Y%m%d_%H_%M")
-    min_col, max_col, min_row, max_row = [int(s) for s in os.path.basename(path).split(".")[0].split("_")[-4:]]
-    label = np.load(path.replace("images", "labels"))
+    distance_distribution = flights.groupby("FL_bin")[["dist_km"]].sum()
 
-    rows, cols = np.where(label > 0)
-    rows += min_row
-    cols += min_col
+    return distance_distribution
 
-
-    suffix = "C" if conus else "F"
-    nc = xr.open_dataset(get_nc_path(time, product="ABI-L2-MCMIP" + suffix))
-
-    x = nc.x.values[cols]
-    y = nc.y.values[rows]
-
-    lons, lats = ABI2geodetic(x, y)
-    
-    itp_coords = {"longitude" : xr.DataArray(lons, dims="s"),
-                  "latitude" : xr.DataArray(lats, dims="s"),
-                  "time" : xr.DataArray(np.array([time] * len(lons)), dims="s")}
-
-    era5 = get_ERA5_data(time)
-    
-    itp = era5.interp(**itp_coords, method="linear")
-    
-     
-    save_path = "/home/vmeijer/contrail-height-estimation/data/weather_itp/" \
-        + os.path.basename(path).replace(".png", ".nc")
-    
-    itp.to_netcdf(save_path)
-    
-
-def get_contrail_pressures(heights, gh_vals, pressures):
-    contrail_pressures = np.zeros_like(heights)
-    
-    for i in range(len(heights)):
-        
-        contrail_pressures[i] = np.interp(heights[i], gh_vals, pressures)
-        
-        
-    return contrail_pressures
-    
-
-    
 
 def process_file(input_path, save_path):
 
